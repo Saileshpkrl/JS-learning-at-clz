547ce1c2d09a17d7f0a94741c65cc0994b405dae
Adhish Pokharel <adhishpokharel105@gmail.com>
2026-02-10 10:10:55 +0545

    first commit

---
+++ myJaS.js
@@ -0,0 +1,57 @@
+// let a = 5
+// let b = 6
+// let sum = a+b 
+// let sub = a -b
+// let mul = a*b
+// let rem = a%b
+// let exponent = a**b
+
+// console.log(sum)
+// console.log(sub)
+// console.log(mul)
+// console.log(rem)
+// console.log(exponent);
+
+// //Assignment operators
+
+// let c = 20
+// c= c+1
+
+// console.log("first", c);
+// c+=1
+// console.log("second", c)
+
+// c-=1
+// console.log("third", c);
+
+// c*=2
+// console.log("fourth", c);
+
+// c%=2
+// console.log("fifth", c);
+
+// c**=3
+// console.log("sixth", c);
+
+
+//comprosion operators
+// let a= 10
+// let b=8
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a>=b);
+// console.log(a<=b);
+
+
+// == check only value but ignores data type
+// let a = 10
+// let b = "10"
+// console.log(a==b)
+// console.log(a===b)
+
+
+
+//write a program to convert the degree celsius to degree fahrenheit [celsius*(9/6)+32]
+let c = 30
+let df =  c*(9/6)+32
+console.log("farenheit = ", df);
