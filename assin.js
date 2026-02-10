5aab0d3c4b04116eabbc4d049c3849b80c5c0274
Adhish Pokharel <adhishpokharel105@gmail.com>
2026-02-11 04:35:22 +0545

    my forst assignment

---
+++ assin.js
@@ -0,0 +1,12 @@
+//Write a JavaScript program to calculate BMI using weight and height and display the BMI value.
+var Weight= parseFloat(prompt("Enter your weight: "));
+var Height= parseFloat(prompt("Enter your height: "));
+let bmi = weight / (height*height);
+console.log("Your BIM is: ", bmi);
+
+
+var weight = 65
+var height = 1.7
+
+let BMI = weight / (height * height)
+console.log("Your weight is: ",BMI.toFixed((2)))
